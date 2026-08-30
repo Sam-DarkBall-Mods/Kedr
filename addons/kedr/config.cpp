bab6f5732f41b57d776ad5cbf7f56898ffd13d81
DarkBall123 <vitalya228007@gmail.com>
2026-08-30 13:50:13 +0300

    Prepare mod for open-source HEMTT releases

--- addons/kedr/config.cpp
+++ addons/kedr/config.cpp
@@ -6,8 +6,8 @@ class CfgPatches
     {
         name = "kedr_antiUAVs";
         author = "Sam/Darkball";
-        requiredVersion = 2;
-        requiredAddons[] = 
+        requiredVersion = 2.22;
+        requiredAddons[] =
         {
             "A3_Weapons_F_Launchers_Titan",
             "A3_Data_F_AoW_Loadorder"
@@ -18,7 +18,7 @@ class CfgPatches
 };
 
 class CfgAmmo
-{    
+{
     class MissileBase;
     class M_Titan_AA: MissileBase
     {
@@ -148,7 +148,7 @@ class CfgMagazines
     class kedr_mag: Titan_AA
     {
         ammo = "Kedr_antiUAV";
-        model = "kedr\mag\kedr_mag.p3d";        
+        model = "kedr\mag\kedr_mag.p3d";
         displayname = "KEDR UAV 1x";
         displayNameShort = "KEDR UAV 1x";
         picture = "\kedr\ui\mag.paa";
@@ -161,15 +161,15 @@ class CfgMagazines
 class CfgWeapons
 {
     class launch_O_Titan_F;
-    
+
     class Kedr_antiUAV: launch_O_Titan_F
     {
         author = "Sem";
         displayname = "KEDR UAV";
         model = "kedr\kedr.p3d";
-        weaponInfoType = "RscWeaponZeroing"; 
-        modelOptics = "";                   
-        useModelOptics = 0;  
+        weaponInfoType = "RscWeaponZeroing";
+        modelOptics = "";
+        useModelOptics = 0;
         handAnim[] = {"OFP2_ManSkeleton", "\kedr\ani\kedr.rtm"};
         picture = "\kedr\ui\body.paa";
         magazines[] = {"kedr_mag"};
@@ -184,7 +184,7 @@ class CfgWeapons
                 effectName = "";
             };
         };
-        
+
         cmImmunity = 1;
         canLock = 2;
         lockAcquire = 1;
@@ -195,7 +195,7 @@ class CfgWeapons
         lockedTargetSound[] = {"kedr\files\2.ogg", 1, 2.5};
 
         modes[] = {"Single"};
-        
+
         // Исправлено: Наследуемся напрямую от режима Single Титана
         class Single: launch_O_Titan_F
         {
@@ -208,7 +208,7 @@ class CfgWeapons
                 begin1[] = {"\kedr\sound\zap.ogg", 2, 1, 2000};
                 soundBegin[] = {"begin1", 1};
             };
-            
+
             reloadTime = 2;
             dispersion = 0.002;
             aiRateOfFire = 7;
@@ -227,24 +227,24 @@ class CfgVehicles
 {
     class Box_EAF_WpsSpecial_F;
     class kedr_box : Box_EAF_WpsSpecial_F
-    {   
+    {
         scope = 2;
         scopecurator = 2;
         author = "Sam";
         displayName = "Kedr box 1x";
 
-        class TransportWeapons 
+        class TransportWeapons
         {
-            class _xx_Kedr_antiUAV 
+            class _xx_Kedr_antiUAV
             {
                 weapon = "Kedr_antiUAV";
                 count = 1;
             };
         };
 
-        class TransportMagazines 
+        class TransportMagazines
         {
-            class _xx_kedr_mag 
+            class _xx_kedr_mag
             {
                 magazine = "kedr_mag";
                 count = 3;
---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "Kedr 1.0.0";
+actionName = "GitHub";
+action = "https://github.com/Sam-DarkBall-Mods/Kedr";
+description = "Kedr - Version 1.0.0";
+tooltip = "Kedr";
+author = "DarkBall, Sam, and contributors";
